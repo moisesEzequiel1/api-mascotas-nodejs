0b5168577dfff6013a1772e72b5f5c0a0eb9ebbe
root <root@DESKTOP-540AM4U.localdomain>
2021-07-22 13:39:41 -0600

    Create-node-app

---
+++ src/app.js
@@ -0,0 +1,18 @@
+// api-mascotas-seleccion-hackademy
+
+const express = require( 'express' );
+const morgan = require( 'morgan' );
+const path = require( 'path' ); 
+const app = express();
+
+app.set('port', process.env.PORT || 3000)
+
+// Middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(morgan('dev'));
+app.use(express.json());
+// Rutas
+app.use('/api/pets/', require('./routes/index'));
+
+app.listen(app.get('port'));
+console.log('server on port: ', app.get('port'));
---
+++ src/routes/index.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+        "projectId": process.env.PROJECT_ID,
+        "private_key": process.env.PRIVATE_KEY,
+        "client_email": process.env.CLIENT_EMAIL
+    }),
+
+    databaseURL: 'https://database-mascotas-default-rtdb.firebaseio.com/'
+});
+const database = admin.database();
+const { Router } = require( 'express' );
+const router = Router();
+
+router.get('/', async (request, response) => {
+    //consultar firebase
+    await database.ref('pets').once( 'value' ,(snapshot) => {
+        data = snapshot.val();
+        response.json(data);
+    });
+
+});
+
+router.post('/new', async (request, response) => {
+        const newPet = {
+            id: request.body.id,
+            petName: request.body.petname,
+            petAge: request.body.petage,
+            petType: request.body.pettype
+        }
+        if (newPet.id && newPet.petName){
+        await database.ref('pets/').push(newPet);
+        response.redirect('/');
+        }else{
+            response.send("Wrong-request");
+        }
+});
+
+router.get('/delete/:id', (request, response) => {
+    database.ref('pets/' + request.params.id).remove();
+    response.redirect('/');
+});
+router.post('/debug', (req, res) => {
+    res.json(req.body);
+});        
+
+module.exports = router;
+
