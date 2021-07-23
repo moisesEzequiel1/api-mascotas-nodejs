59f25c1780294b9a50c566b2525d435820d4b69b
Moises Dominguez <dominguezsalcedom@gmail.com>
2021-07-22 18:08:52 -0600

    fix-morgan-debug

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 // api-mascotas-seleccion-hackademy
 
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
 
 app.set('port', process.env.PORT || 3000)
 
 // Middlewares
 app.use(express.urlencoded({extended: false}));
-app.use(morgan('dev'));
 app.use(express.json());
 // Rutas
 app.use('/api/pets/', require('./routes/index'));
